819a081c95753a6eec30a319d56203be07f20ddf
nmcginn <2986379+nmcginn@users.noreply.github.com>
2021-12-23 21:11:33 -0600

    fix directory

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import { APIGatewayProxyResult } from "aws-lambda";
 
 export const handler = async(): Promise<APIGatewayProxyResult> => {
-    const files = fs.readdirSync('./');
+    const files = fs.readdirSync('/opt/');
     console.log(files);
     return {
         statusCode: 200,
