7dc89d784453fc64bc95827bee7dc48096747878
nmcginn <2986379+nmcginn@users.noreply.github.com>
2021-12-24 13:37:34 -0600

    basic test

--- test/chessapi.test.ts
+++ test/chessapi.test.ts
@@ -1,17 +1,7 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as chessapi from '../lib/chessapi-stack';
+import * as api from '../api';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/chessapi-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new chessapi.ChessapiStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+it('Invokes stockfish with default command', async() => {
+    const result = await api.handler();
+    expect(result.statusCode).toEqual(200);
+    expect(result.body).toBeDefined();
 });
